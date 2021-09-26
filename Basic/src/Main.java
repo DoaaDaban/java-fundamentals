e2157c6ab1616eb33806c0170725dbbd5ef93183
DoaaDaban <doaadaban993@gmail.com>
2021-09-26 16:17:37 +0300

    Pluralize2

---
+++ Basic/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        int dogCount = 1;
+        System.out.println("I own " + dogCount + " " + pluralize("dog", dogCount) + ".");
+
+        int catCount = 2;
+        System.out.println("I own " + catCount + " " + pluralize("cat", catCount) + ".");
+
+        int turtleCount = 0;
+        System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
+
+    }
+
+    public static String pluralize(String animalName, int animalNum) {
+        if (animalNum > 1 || animalNum == 0) {
+            return animalName + "s";
+        } else {
+            return animalName;
+        }
+    }
+}
+
